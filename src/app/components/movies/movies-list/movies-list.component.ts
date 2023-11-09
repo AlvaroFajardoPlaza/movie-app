1556a1ee8d143e230ec0451fb5743194d3ef7ee0
alvaro <alvaro.fajardoplaz@gmail.com>
2023-11-09 09:58:44 +0100

    observable tryout en MoviesListComponent.

--- src/app/components/movies/movies-list/movies-list.component.ts
+++ src/app/components/movies/movies-list/movies-list.component.ts
@@ -1,34 +1,44 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { MoviesService } from 'src/app/services/movies.service';
 import { Movie } from 'src/app/models/movie.interface';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription, filter, map, switchMap } from 'rxjs';
 
 @Component({
 	selector: 'app-movies-list',
 	templateUrl: './movies-list.component.html',
 	styleUrls: ['./movies-list.component.scss']
 })
-export class MoviesListComponent implements OnInit {
-	movies: Movie[] = [];
+export class MoviesListComponent {
+	movies: Array<Movie> = [];
 	movie: Movie | any;
-	subscription: Subscription;
+	subscription: Subscription; // Esta subscription nos es útil al manejar el ciclo de vida del componente.
 
-	constructor(private movieService: MoviesService, private router: Router) {}
+	// Al inyectar los servicios, no es necesario emplear el constructor del componente
+	private _router = inject(Router);
+	private _activatedRouter = inject(ActivatedRoute);
+	private _movieService = inject(MoviesService);
 
-	ngOnInit(): void {
-		this.movieService.getAll().subscribe((movies: any) => {
+	moviesList$: Observable<Array<Movie>> = this._movieService.getAll();
+
+	// Esperamos a que cargue el componente antes de iniciar el Observable
+	ngOnInit() {
+		this.subscription = this.moviesList$.subscribe((movies: any) => {
 			this.movies = movies;
-			console.log('recibimos las películas? ', movies);
+			console.log('Listado de películas: ', movies);
 		});
 	}
 
+	// Primer componente lista de películas
+	// ngOnInit(): void {
+	// 	this.movieService.getAll().subscribe((movies: any) => {
+	// 		this.movies = movies;
+	// 		console.log('recibimos las películas? ', movies);
+	// 	});
+	// }
+
 	// Ruta para navegar a los datos de 1 película
 	MovieInfo(id: Number) {
-		this.router.navigate(['explore-movies', id]);
+		this._router.navigate(['explore-movies', id]);
 	}
-
-	// ngOnDestroy(): void {
-	// 	this.subscription.unsubscribe();
-	// }
 }
